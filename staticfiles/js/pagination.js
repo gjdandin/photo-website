0fd3a42e86d028cc1d3b92b96dcc9c4660203e44
Gilhan Jentrix <gilhan_norway@hotmail.com>
2021-06-04 23:02:46 +0200

    Removed staticfiles from git repo

--- staticfiles/js/pagination.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).on('click','.load-more',function(e){
-    e.preventDefault();
-    jQuery.ajax({
-        url:jQuery(this).attr('href')
-    }).done(function(data){
-        jQuery('.row>div').append(data);
-    });
-});
